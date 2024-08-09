9ac278b80782ef4b1cfdfc24ff39c97e114f8c5c
AbidiJihed1 <abidi-jihed@outlook.com>
2024-08-09 10:50:46 +0100

    save

--- maitrelaribi/src/components/Maps.js
+++ maitrelaribi/src/components/Maps.js
@@ -22,16 +22,7 @@ const Maps = () => {
             </p><br />
           </div>
     <div className='col-7'>
-      <iframe
-        title="Google Maps Location"
-        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d798.6815031746128!2d10.165453769626946!3d36.801119072114574!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x12fd338b9541419b%3A0xda249182190fc488!2s44%20Bd%20Bab%20Benet%2C%20Tunis!5e0!3m2!1sfr!2stn!4v1707063553101!5m2!1sfr!2stn"
-        width="100%"
-        height="300"
-        style={{ border: 0 }}
-        allowFullScreen=""
-        loading="lazy"
-        referrerPolicy="no-referrer-when-downgrade"
-      ></iframe>
+    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1316.6771650450926!2d10.181933999999991!3d36.7952653!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x12fd35001508983f%3A0x23d1c0bf07e45964!2sCarthage%20Center!5e1!3m2!1sfr!2stn!4v1723196973096!5m2!1sfr!2stn" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
       </div>
     </div>
   );
