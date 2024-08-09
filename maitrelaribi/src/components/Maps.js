ec18bdbcd3d59c6d4036c202ba704198b49e6e27
AbidiJihed1 <abidi-jihed@outlook.com>
2024-08-09 10:52:59 +0100

    save

--- maitrelaribi/src/components/Maps.js
+++ maitrelaribi/src/components/Maps.js
@@ -22,7 +22,7 @@ const Maps = () => {
             </p><br />
           </div>
     <div className='col-7'>
-    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1316.6771650450926!2d10.181933999999991!3d36.7952653!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x12fd35001508983f%3A0x23d1c0bf07e45964!2sCarthage%20Center!5e1!3m2!1sfr!2stn!4v1723196973096!5m2!1sfr!2stn" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+      <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1316.6771650450926!2d10.181933999999991!3d36.7952653!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x12fd35001508983f%3A0x23d1c0bf07e45964!2sCarthage%20Center!5e1!3m2!1sfr!2stn!4v1723196973096!5m2!1sfr!2stn" width="600" height="450" style={{border:0}} allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
       </div>
     </div>
   );
