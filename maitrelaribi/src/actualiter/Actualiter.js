91034d865fa665490734bf35cde53e2bdc969c81
AbidiJihed1 <abidi-jihd@outlook.com>
2024-09-11 15:19:34 +0100

    save

--- maitrelaribi/src/actualiter/Actualiter.js
+++ maitrelaribi/src/actualiter/Actualiter.js
@@ -30,7 +30,7 @@ useEffect(()=>{
             </div>
           </div>
           <div className="post-content">
-            {post.imageUrl !==null ? <img className="post-image" src={post.imageUrl}  />:null}
+            {post.imageUrl !== null ? <img className="post-image" src={post.imageUrl}  /> :<div></div> }
             {post.fileUrl && post.fileUrl.endsWith('.pdf') && (
             <iframe
               src={post.fileUrl}
